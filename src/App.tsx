fdbcecca88bf373765d998169e67f573bd57848d
AlexByte <alex.g.cortes.g@gmail.com>
2024-04-11 00:43:38 -0600

    config navigation

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { NavigationContainer } from '@react-navigation/native'
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const App = () => {
+  return (
+    <NavigationContainer>
+        <View>
+            <Text>App</Text>
+        </View>
+    </NavigationContainer>
+  )
+}
+
+export default App
